9ba6d382303805d2dc994eef134d9fe10aa9481d
gcvictor22 <victorgonzaleztr22@gmail.com>
2022-10-11 19:33:52 +0200

    First commit

---
+++ ejercicioSWAPI/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: 'main-menu', component: },
+  {path: '', redirectTo: '/main-menu', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
