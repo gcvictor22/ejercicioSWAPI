eec1662b5ab5471758c1a6126bcfbcedcb52aac2
gcvictor22 <victorgonzaleztr22@triana.salesianos.edu>
2022-10-14 10:31:36 +0200

    Characters terminado

--- ejercicioSWAPI/src/app/app-routing.module.ts
+++ ejercicioSWAPI/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './components/characters/characters.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'inicio', component: MainMenuComponent},
-  {path: 'personajes', component: MainMenuComponent},
+  {path: 'characters', component: CharacterComponent},
   {path: 'films', component: MainMenuComponent},
   {path: 'species', component: MainMenuComponent},
   {path: 'starships', component: MainMenuComponent},
