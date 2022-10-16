d4b5eeeab411d86a9e1055c6cf4e7e4b214b7fdd
Antonio Montero <antoniomonforma99@gmail.com>
2022-10-16 20:49:35 +0200

    starhips

--- ejercicioSWAPI/src/app/app-routing.module.ts
+++ ejercicioSWAPI/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { StarshipsComponent } from './components/starships/starships.component';
 
 const routes: Routes = [
   {path: 'inicio', component: MainMenuComponent},
   {path: 'personajes', component: MainMenuComponent},
   {path: 'films', component: MainMenuComponent},
   {path: 'species', component: MainMenuComponent},
-  {path: 'starships', component: MainMenuComponent},
+  {path: 'starships', component: StarshipsComponent},
   {path: 'vehicles', component: MainMenuComponent},
   {path: 'planets', component: MainMenuComponent},
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
---
+++ ejercicioSWAPI/src/app/components/starships/starships.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Starship } from 'src/app/interfaces/starship.interface';
+import { StarshipService } from 'src/app/services/starship.service';
+
+
+const URL_IMAGEN = 'https://starwars-visualguide.com/assets/img/planets/'
+
+@Component({
+  selector: 'app-starships',
+  templateUrl: './starships.component.html',
+  styleUrls: ['./starships.component.css']
+})
+export class StarshipsComponent implements OnInit {
+  
+  starshipsList: Starship[] = [];
+  numPage = 0;
+  selected = false;
+
+  constructor(private starshipService: StarshipService) { }
+
+  ngOnInit(): void {
+    this.getStarshipPage(1);
+  }
+
+  getStarshipPage(page: number){
+    this.starshipService.getStarshipsList(page).subscribe(resp => {
+      this.starshipsList = resp.results;
+      this.numPage = Math.ceil(resp.count /10);
+    })
+  }
+
+  getStarshipPhoto(starship: Starship) {
+    let id = starship.url.split('/')[5];
+    return `${URL_IMAGEN}${id}.jpg`
+  }
+
+  counter(){
+    return new Array(this.numPage);
+  }
+
+  
+
+}
